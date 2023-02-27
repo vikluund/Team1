d97308c7c881ee3e257384d5988881c572dad9c7
Tim <112405922+vikluund@users.noreply.github.com>
2023-02-27 13:24:49 +0100

    fixed HomeView

--- aventuras/src/App.vue
+++ aventuras/src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
-import HeroImageTest from "./components/HeroImageTest.vue"
+
 
 </script>
 
 <template>
   <NavBar />
-  <HeroImageTest />
-  </template>
+</template>
