f850acad1b6f33e10f55ca35076f78cde36c5483
Tim <112405922+vikluund@users.noreply.github.com>
2023-03-08 22:47:57 +0100

    Added to the product page

--- aventuras/src/views/ProductView.vue
+++ aventuras/src/views/ProductView.vue
@@ -13,8 +13,11 @@ import ProductImage from "../components/ProductImage.vue"
     <img src="../assets/media/alessio-soggetti-xqBllPa7XEs-unsplash.jpg" alt="Product Image 1" class="theImage">
   </div>
   <div class="productsTwo">
-    <img src="../assets/media/glade-optics-ttGLlNElbCc-unsplash.jpg" alt="Product Image 1" class="theImage">
+  <div class="productsTwo-text"><h2>Bring your friends and family</h2></div>
+    <div class="overlay"></div>
+    <img src="../assets/media/glade-optics-ttGLlNElbCc-unsplash.jpg" alt="Product Image 1">
   </div>
+  <div class="booking"></div>
 </template>
 
 <style scoped>
@@ -23,7 +26,7 @@ import ProductImage from "../components/ProductImage.vue"
   text-align: center;
   position: relative;
   padding-top: 5vh;
-  font-size: 4em;
+  font-size: 6vw;
   font-family: 'Advent Pro';
   text-shadow: 1px 1px 2px #000000a1;
 }
@@ -35,26 +38,119 @@ import ProductImage from "../components/ProductImage.vue"
   padding-bottom: 10vh;
 }
 .theImage {
-  width: 600px;
+  width: 40vw;
   height: auto;
   margin-right: 20px;
-  border-radius: 3%;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.4);
 }
 .productText {
   margin: 8vw;
-  padding: 2vw;
+  padding-left: 2vw;
+  padding-bottom: 2vw;
   flex: 1;
   max-width: 800px;
   line-height: 1.4;
-  border-left: 4px solid rgb(12, 150, 177);
-  border-bottom: 4px solid rgb(12, 150, 177);
+  border-left: 4px solid transparent;
+  border-bottom: 4px solid transparent;
+  border-image: linear-gradient(to bottom right, rgb(15, 115, 11), rgb(255, 255, 255)) 1 5%;
+  /* border-left: 4px solid rgb(15, 115, 11);
+  border-bottom: 4px solid rgb(15, 115, 11); */
   font-family: 'Montserrat';
+}
+.productText h2 {
+  font-size: 2vw;
+  padding-bottom: 5vh;
+  margin-top: 0;
+  font-weight: 500;
+}
+.productText p {
+  font-size: 1vw;
 }
 .productsTwo {
   display: flex;
+  justify-content: center;
   align-items: center;
-  padding-top: 10vh;
-  padding-left: 6vw;
-  padding-bottom: 10vh;
+  overflow: hidden;
+  height: 90vh;
+  margin-bottom: 10vh;
+  position: relative;
+}
+.productsTwo-text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  width: 24vw;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border: 0.4vw solid rgb(255, 255, 255);
+  position: absolute;
+  top: 50%;
+  left: 85%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+}
+.productsTwo-text h2 {
+  font-size: 2.5em;
+  font-weight: 600;
+  letter-spacing: 0.05em;
+  text-shadow: 2px 2px 18px #000000a1;
+  font-family: 'Montserrat';
+}
+.productsTwo img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.booking {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10%;
+  height: 80vh;
+  width: 80vw;
+  background-color: rgb(57, 57, 57);
+}
+@media (max-width: 768px){
+  .products {
+    padding-left: 0;
+    flex-direction: column;
+  }
+  .theImage {
+    width: 80%;
+    margin: 0;
+  }
+  .productText h2{
+    font-size: 20px;
+  }
+  .productText p {
+    font-size: 16px;
+  }
+  .productsTwo {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  .productsTwo img {
+    margin: 0 auto;
+    height: auto;
+    max-width: 100%;
+    overflow-x: hidden;
+  }
+
 }
 </style>
