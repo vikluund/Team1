4eeb6c134eb4f6e979dd7f88419c86284cad49df
Melissa Gonzalez <mel.silgon@gmail.com>
2023-03-08 10:37:49 +0100

    Finished SeasonsMenu & imported it in HomeView
    Plus corrected seasonmenu-summer img name.

--- aventuras/src/views/HomeView.vue
+++ aventuras/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HeroImageTest from '../components/HeroImageTest.vue'
+import SeasonsMenu from '../components/SeasonsMenu.vue'
 import ActCards from '../components/ActCards.vue'
 </script>
 
 <template>
   <HeroImageTest />
+  <SeasonsMenu />
   <ActCards />
 </template>
 
