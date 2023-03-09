f634c369a2153a03619b280388d40d6bc896ac21
Venessa Karlsson <nessy.right@live.se>
2023-03-09 17:51:06 +0100

    Added new cards

--- aventuras/src/views/HomeView.vue
+++ aventuras/src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup>
 import HeroImageTest from '../components/HeroImageTest.vue'
-import SeasonsMenu from '../components/SeasonsMenu.vue'
+// import SeasonsMenu from '../components/SeasonsMenu.vue'
 import NewCards from '../components/NewCards.vue'
 </script>
 
 <template>
   <HeroImageTest />
-  <ActCards />
+  <NewCards/>
 </template>
 
 <style scoped></style>
